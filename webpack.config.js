aac14f47a9636b0cd6cb8948a28e3ec6e51509af
Marilyn <kiradeveloppeuse@gmail.com>
2023-03-19 17:52:44 +0100

    suppression des modals

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('modal-confirmation', './assets/modal-confirmation.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
