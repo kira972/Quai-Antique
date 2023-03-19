6cb4c43262464718f98922c9c899f6274c2798cd
Marilyn <kiradeveloppeuse@gmail.com>
2023-03-19 16:25:19 +0100

    heroku

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,16 +2,12 @@
 
 namespace App\Controller;
 
-use id;
 use App\Entity\User;
 use App\Entity\Reservation;
 use App\Form\ReservationType;
-use App\Repository\UserRepository;
-use App\Repository\AllergieRepository;
 use App\Repository\RestaurantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\OpeningTimeRepository;
-use App\Repository\ReservationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,36 +16,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ReservationController extends AbstractController
 {
-
-
-    #[Route('/reservation-info/{id}', name: 'app_reservation-info', methods : ['GET', 'POST'])]
-    public function getReservationInfo( UserRepository $userRepository )
-    {
-        //$repository = json_decode($userRepository->getContent($id), true);
-        
-        
-        // $userRepository = $id['date'];
-        // $reservationTime = $['time'];
-        // $numGuests = $repositorynNumberCover['num_guests'];
-        // $reservationName = $requestData['reservation_name'];
-        // $allergies = $repositoryAllergie['allergies'];
-
-        // Effectuer les traitements nécessaires
-
-        // Retourner les données au format JSON 
-        $data = [
-            //'user' => $repository
-        //   'date' => $reservationDate,
-        //   'time' => $reservationTime,
-        //   'num_guests' => $numGuests,
-        //   'reservation_name' => $reservationName,
-        //   'allergies' => $allergies,
-        ];
-
-        return $this->json($data);
-    }
-
-
     #[Route('/reservation', name: 'app_reservation')]
     public function index( 
         Request $request,
@@ -91,8 +57,6 @@ class ReservationController extends AbstractController
             $manager->persist($reservation);
             $manager->flush();
 
-            // return $this-> redirectToRoute('app_home');      
-            return $this-> redirectToRoute('app_reservation-info');      
         }
 
         return $this->render('pages/reservation/index.html.twig', [
@@ -101,7 +65,4 @@ class ReservationController extends AbstractController
             'restaurant' => $restaurant,
         ]);
     }
-
-
-    
 }
